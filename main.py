5597a29d7d84ad8c23e8e97266524aad2d63fcd5
Marcel Nunez <mpnunez28@gmail.com>
2022-06-26 20:12:13 -0700

    This is hopeless

--- main.py
+++ main.py
@@ -1,79 +1,58 @@
 #! ./.venv/bin/python
 
 """
+Modified from
 https://docs.dask.org/en/latest/deploying-python-advanced.html
 """
 
 import asyncio
 from dask.distributed import Scheduler, Worker, Client
-
-from contextlib import AsyncExitStack
-
 import sys
 from pathlib import Path
-
 import subprocess
 
-class ClusterWorker:
-    """
-    Launches a worker on another machine
+WORKER_CONNECT_WAIT = 3     # seconds after worker process starts to connect to scheduler
 
-    Need to customize this class for your HPC cluster
-    """
-
-    def __init__(self,scheduler_address=None):
-        self.process = None
-        self.scheduler_address = scheduler_address
-
-    async def __aenter__(self):
-        """
-        Launch the worker
-        """
-
-        worker_exe = Path(sys.executable).parent / "dask-worker"
-
-        #worker_cmd = [f"{worker_exe}", "--no-reconnect", f"{self.scheduler_address}"]
-        worker_cmd = [f"{worker_exe}", f"{self.scheduler_address}"]
-        #worker_cmd = [f"{worker_exe}", "--no-reconnect", "--death-timeout '30'", f"{self.scheduler_address}"]
-        print(worker_cmd)
-        #self.process = await asyncio.create_subprocess_exec(*worker_cmd)
-        self.process = subprocess.Popen(f"{worker_exe} {self.scheduler_address}",shell=True)
-        print("RETURN CODE: "+ str(self.process.returncode))
-        return self.process
-
-
-    async def __aexit__(self, exc_type, exc, tb):
-        print("EXIT CONTEXT FOR WORKER")
-        if self.process.returncode is None:
-            pass
-            print("KILLING SUBPROCESS")
-            self.process.kill()
 
 async def f():
+    async with Scheduler(scheduler_file="scheduler_info.json") as s:
+        async with Client(s.address, asynchronous=True) as client:
 
-    async with AsyncExitStack() as stack:
-        
-        # Start scheduler
-        s = Scheduler(dashboard=True)
-        await stack.enter_async_context(s)  # Scheduler must be running before client or workers connect
-        print(s.address)
+            # Start worker in separate python process
+            worker_exe = Path(sys.executable).parent / "dask-worker"
+            worker_cmd = [f"{worker_exe} {s.address}"]
+            
 
-        # Start client
-        client = Client(s.address, asynchronous=True)
-        await stack.enter_async_context(client)
-
-        async with AsyncExitStack() as worker_stack:
-
-            # Start workers
-            n_workers = 5
-            child_procs = [await worker_stack.enter_async_context(ClusterWorker(s.address)) for _i in range(n_workers)]
-            #child_procs = [await stack.enter_async_context(Worker(s.address)) for _i in range(n_workers)]
-
-            #await client_fut
             future = client.submit(lambda x: x + 1, 10)
             result = await future
+
+            """
+            #worker_process = subprocess.Popen(worker_cmd,shell=True)
+            await asyncio.sleep(3)
+            print(s.workers)
+
+            await asyncio.sleep(3)
+            print(s.workers)
+
+            workers = list(s.workers)
+
+            
             print("Computation finished!")
             print(result)
+
+            # Get workers and retire them
+            print(s.workers)
+            workers = list(s.workers)
+            print("WORKERS")
+            print(workers)
+            await s.retire_workers(s.workers.keys(), close_workers=True)
+            #await asyncio.sleep(3)
+            
+            #worker_process.kill()
+            print("SHOULD EXIT NOW")
+            """
+
+asyncio.get_event_loop().run_until_complete(f())
 
 
 
